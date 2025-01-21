69bb45964b8e76e1d3b2d4f5dbcfd4e070aef879
chinju.p <chinchu.p@wrenchsolutions.com>
2025-01-21 10:41:26 +0530

    complted

---
+++ Task_ManagementApp_React/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TaskProvider } from './context/TaskContext';
+import Dashboard from './pages/Dashboard';
+import TaskForm from './Components/TaskForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const App: React.FC = () => {
+  return (
+    <TaskProvider>
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">Task Manager</h1>
+        <TaskForm />
+        <Dashboard />
+      </div>
+    </TaskProvider>
+  );
+};
+
+export default App;
